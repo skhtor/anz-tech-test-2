dae9c301218b28838f06e68404a14c893f0baa58
Sassoon Kuyumcian <sassoon.kuyumcian@auspost.com.au>
2019-03-02 14:52:28 +1100

    Added healthcheck endpoint

--- main.go
+++ main.go
@@ -7,7 +7,26 @@ func index_handler(w http.ResponseWriter, r *http.Request)  {
   fmt.Fprintf(w, "Hello, world!")
 }
 
+func healthcheck_handler(w http.ResponseWriter, r *http.Request)  {
+  w.WriteHeader(http.StatusOK)
+  w.Header().Set("Content-Type", "application/json")
+
+  payload:= `
+  {
+    "myapplication": [
+      {
+        "version": "1",
+        "description": "pre-interview technical test",
+        "lastcommitsha": "abc123"
+      }
+    ]
+  }`
+
+  fmt.Fprintf(w, payload)
+}
+
 func main()  {
   http.HandleFunc("/", index_handler)
+  http.HandleFunc("/healthcheck/", healthcheck_handler)
   http.ListenAndServe(":8000", nil)
 }
